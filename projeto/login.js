74e693cd469498a495b1bd70a766dfc4d162676b
Evandro Araújo Luz <149076775+d3vluz@users.noreply.github.com>
2024-04-19 00:12:57 -0300

    login/cadastro-nova-palheta

--- projeto/login.js
+++ projeto/login.js
@@ -1,17 +1,19 @@
 var formSignin = document.querySelector("#signin");
 var formSignup = document.querySelector("#signup");
 var btnColor = document.querySelector(".btnColor");
+var btnSignin = document.querySelector("#btnSignin");
+var btnSignup = document.querySelector("#btnSignup");
 
 document.querySelector('#btnSignin').addEventListener('click', () => {
     document.getElementById("signup").style.display = "none";
     document.getElementById("signin").style.display = "block";
     btnColor.style.left = "0px";
-    btnColor.style.background = "linear-gradient(to left, #D6F4D2, #08c256)";
+    btnColor.style.background = "linear-gradient(to right, var(--cor-tres), var(--cor-quatro));"
 });
 
 document.querySelector('#btnSignup').addEventListener('click', () => {
     document.getElementById("signin").style.display = "none";
     document.getElementById("signup").style.display = "block";
     btnColor.style.left = "110px";
-    btnColor.style.background = "linear-gradient(to right, #D6F4D2, #08c256)";
+    btnColor.style.background = "linear-gradient(to left, var(--cor-tres), var(--cor-quatro));";
 });
