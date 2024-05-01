c77c8d6997be84ab45c5c7f86143f28cbd27389c
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2024-05-01 18:33:57 -0300

    Criando o backend do carrinho

---
+++ projeto/assets/script/shopscript.js
@@ -0,0 +1,62 @@
+async function inicializarLoja() {
+    try {
+        const response = await fetch('/api/produtos');
+        const produtos = await response.json();
+        
+        var containerProdutos = document.getElementById('produtos');
+        
+        produtos.forEach((produto) => {
+            containerProdutos.innerHTML += `
+                <div class="produto-single">
+                    <img src="${produto.img}" />
+                    <p>${produto.nome}</p>
+                    <a key="${produto._id}" href="#" class="add-to-cart">Adicionar ao carrinho!</a>
+                </div>
+            `;
+        });
+
+        // Adicionar evento de clique aos links "Adicionar ao carrinho"
+        var links = document.getElementsByClassName('add-to-cart');
+        Array.from(links).forEach((link) => {
+            link.addEventListener("click", async function(event) {
+                event.preventDefault();
+                const produtoId = this.getAttribute('key');
+                
+                // Enviar solicitação para adicionar o produto ao carrinho
+                const response = await fetch('/api/produtos/add', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ produtoId })
+                });
+                
+                // Atualizar o carrinho na interface do usuário
+                atualizarCarrinho();
+            });
+        });
+    } catch (error) {
+        console.error('Erro ao inicializar a loja:', error);
+    }
+}
+
+// Função para atualizar o carrinho na interface do usuário
+async function atualizarCarrinho() {
+    try {
+        const response = await fetch('/carrinho/api/produtos/add');
+        const carrinho = await response.json();
+        
+        var containerCarrinho = document.getElementById('carrinho');
+        containerCarrinho.innerHTML = "";
+        
+        carrinho.forEach((item) => {
+            containerCarrinho.innerHTML += `
+                <p>${item.nome} | Quantidade: ${item.quantidade}</p>
+            `;
+        });
+    } catch (error) {
+        console.error('Erro ao atualizar o carrinho:', error);
+    }
+}
+
+window.onload = inicializarLoja;
--- projeto/index.js
+++ projeto/index.js
@@ -1,105 +1,158 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+      var express = require("express")
+      var bodyParser = require("body-parser")
+      var mongoose = require("mongoose")
 
-const app = express()
+      const app = express()
 
-app.use(bodyParser.json())
-app.use(express.static('assets'))
-app.use('/assets/estilos', express.static(__dirname + '/assets/estilos'));
-app.use('/assets/imagens', express.static(__dirname + '/assets/imagens'));
-app.use('/assets/paginas', express.static(__dirname + '/assets/paginas'));
-app.use('/assets/scripts', express.static(__dirname + '/assets/scripts'));
+      app.use(bodyParser.json())
+      app.use(express.static('assets'))
+      app.use('/assets/estilos', express.static(__dirname + '/assets/estilos'));
+      app.use('/assets/imagens', express.static(__dirname + '/assets/imagens'));
+      app.use('/assets/paginas', express.static(__dirname + '/assets/paginas'));
+      app.use('/assets/scripts', express.static(__dirname + '/assets/scripts'));
 
-app.use(bodyParser.urlencoded({
-      extended: true
-}))
+      app.use(bodyParser.urlencoded({
+            extended: true
+      }))
 
-_promise = mongoose.connect("mongodb://localhost:27017/myDB");
-var db = mongoose.connection;
+      _promise = mongoose.connect("mongodb://localhost:27017/myDB");
+      var db = mongoose.connection;
 
-db.on("error", () => console.log("Erro em conectar ao banco de dados"))
-//db.once("open", () => console.log("Susseco em conectar ao banco de dados"))
-db.once("open", () => {
-      db.collection('users').find({}).toArray()
-      .then((result) => {
-          console.log("Documentos encontrados na coleção 'users':", result);
+      db.on("error", () => console.log("Erro em conectar ao banco de dados"))
+      //db.once("open", () => console.log("Susseco em conectar ao banco de dados"))
+      db.once("open", () => {
+            db.collection('users').find({}).toArray()
+            .then((result) => {
+            console.log("Documentos encontrados na coleção 'users':", result);
+            })
+            .catch((error) => {
+            console.error("Erro ao buscar documentos:", error);
+            });
       })
-      .catch((error) => {
-          console.error("Erro ao buscar documentos:", error);
+
+      app.post("/signup", (req, res) => {
+            var name = req.body.nome
+            var last_name = req.body.sobrenome
+            var email = req.body.email
+            //var cell_number = req.body.celular
+            var password = req.body.senha
+
+            var dataobj = {
+                  "name": name + ' ' + last_name,
+                  "email": email,
+                  //"cell": cell_number,
+                  "password": password
+            }
+
+            db.collection('users').insertOne(dataobj, (err) => {
+                  if (err) {
+                        throw err;
+                  }
+                  console.log("Registro inserido com cusseco")
+            })
+
+            return res.redirect('index.html');
+      })
+
+      app.post("/signin", (req, res) => {
+            
+            var email = req.body.email
+            var password = req.body.senha
+
+            dataobj = {
+                  "email": email,
+                  "password": password
+            }
+
+            db.collection('users').findOne(dataobj, (err, collection) => {
+                  if (err) {
+                        throw err;
+                  }
+                  if (collection) {   
+                        console.log("Usuario encontrado")
+                        return res.redirect('index.html')
+                  } else {
+                        console.log("Usuario nao encontrado")
+                  }
+            })
       });
-})
 
-app.post("/signup", (req, res) => {
-      var name = req.body.nome
-      var last_name = req.body.sobrenome
-      var email = req.body.email
-      //var cell_number = req.body.celular
-      var password = req.body.senha
+      app.get("/loginpage.html", (req, res) => {
+            res.sendFile(__dirname + '/assets/paginas/loginpage.html');
+            console.log("loginpage")
+      });
 
-      var dataobj = {
-            "name": name + ' ' + last_name,
-            "email": email,
-            //"cell": cell_number,
-            "password": password
-      }
+      app.get("/index.html", (req, res) => {
+            res.sendFile(__dirname + '/index.html');
+            console.log("index")
+      });
 
-      db.collection('users').insertOne(dataobj, (err) => {
-            if (err) {
-                  throw err;
-            }
-            console.log("Registro inserido com cusseco")
-      })
+      //app.get("/home.css", (req, res) => {
+      //      res.sendFile(__dirname + '/assets/estilos/home.css');
+      //});
+      //app.get("/index.html", (req, res) => {
+      //      res.sendFile(__dirname + '/index.html');
+      //});
 
-      return res.redirect('index.html');
-})
 
-app.post("/signin", (req, res) => {
+
+      // Modelos de dados e rotas para manipular o carrinho
+      const itemSchema = new mongoose.Schema({
+            nome: String,
+            img: String,
+            quantidade: Number
+      });
       
-      var email = req.body.email
-      var password = req.body.senha
-
-      dataobj = {
-            "email": email,
-            "password": password
-      }
-
-      db.collection('users').findOne(dataobj, (err, collection) => {
-            if (err) {
-                  throw err;
+      const Item = mongoose.model("Item", itemSchema);
+      
+      // Rota para adicionar um item ao carrinho
+      app.post("/api/produtos/add", async (req, res) => {
+            const { nome, img, quantidade } = req.body;
+            
+            try {
+            const newItem = new Item({ nome, img, quantidade });
+            await newItem.save();
+            res.status(201).json(newItem);
+            } catch (err) {
+            res.status(400).json({ message: err.message });
             }
-            if (collection) {   
-                  console.log("Usuario encontrado")
-                  return res.redirect('index.html')
-            } else {
-                  console.log("Usuario nao encontrado")
+      });
+      
+      // Rota para obter todos os itens do carrinho
+      app.get("/api/produtos", async (req, res) => {
+            try {
+                const products = await Product.find();
+                res.json(products);
+            } catch (err) {
+                res.status(500).json({ message: err.message });
             }
-      })
-});
-
-app.get("/loginpage.html", (req, res) => {
-      res.sendFile(__dirname + '/assets/paginas/loginpage.html');
-      console.log("loginpage")
-});
-
-app.get("/index.html", (req, res) => {
-      res.sendFile(__dirname + '/index.html');
-      console.log("index")
-});
-
-//app.get("/home.css", (req, res) => {
-//      res.sendFile(__dirname + '/assets/estilos/home.css');
-//});
-//app.get("/index.html", (req, res) => {
-//      res.sendFile(__dirname + '/index.html');
-//});
-
-app.get("/", (req, res) => {
-      res.set({
-            "ALLow-access-ALLow-Origin": "*"
-      })
-      return res.redirect("loginpage.html");
-}).listen(3000)
+        });
+      
+      // Rota para remover um item do carrinho
+      app.delete("/api/produtos/:id", async (req, res) => {
+            const { id } = req.params;
+            
+            try {
+            await Item.findByIdAndDelete(id);
+            res.json({ message: "Item removido do carrinho com sucesso" });
+            } catch (err) {
+            res.status(500).json({ message: err.message });
+            }
+      });
+  
 
 
-console.log("Ouvindo porta 3000...")
+
+
+
+
+
+      app.get("/", (req, res) => {
+            res.set({
+                  "ALLow-access-ALLow-Origin": "*"
+            })
+            return res.redirect("loginpage.html");
+      }).listen(3000)
+
+
+      console.log("Ouvindo porta 3000...")
